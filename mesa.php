0d704dabc426a27a171712ef7359dced259e43a3
NeoDaark <DaarkAssassin@gmail.com>
2016-04-21 19:39:51 +0200

    Css, Theme, Index, Menu

---
+++ mesa.php
@@ -0,0 +1,19 @@
+<!-- Header content box -->
+<?php 
+$title='Index';
+$migas='#Index|index.php';
+include "Public/layouts/head.php";?>
+      
+<!-- Body content box -->
+<div class="container theme-showcase" role="main"> <!-- container Start -->
+    
+    
+    
+    
+    
+    
+</div><!-- /container -->
+
+<!-- Footer content box -->
+<?php include "Public/layouts/footer.php";?> 
+
