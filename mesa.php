1a6c54591479f322a53160f16d1ba88f70b7b9b6
NeoDaark <DaarkAssassin@gmail.com>
2016-04-25 23:23:13 +0200

    mi mesa html

--- mesa.php
+++ mesa.php
@@ -34,16 +34,55 @@ include "Public/layouts/head.php";?>
         <!-- /#sidebar-wrapper -->
 
         <!-- Page Content -->
-        <div class="bg-color1" id="page-content-wrapper">
+        <div  id="page-content-wrapper">
             <div class="container-fluid ">
                 <div class="row">
-                    <div class="col-lg-12 ">
-                        <h1>Simple Sidebar</h1>
+                    <ul class="nav nav-tabs ">
+                        <li role="presentation" class="active"><a href="#">Home</a></li>
+                        <li role="presentation"><a href="#">Profile</a></li>
+                        <li role="presentation"><a href="#">Messages</a></li>
+                        <li role="presentation" class="dropdown">
+                          <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                            Dropdown <span class="caret"></span>
+                          </a>
+                          <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">One more separated link</a></li>
+                          </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="row bg-color2">
+                    <div class="col-md-8 bg-color1">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1>User Panel</h1>
+                                <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
+                                <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                                <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+                            </div>
+                        </div>
+                        <div class="clearfix visible-xs"></div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1>Map Panel</h1>
+                                <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
+                                <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 bg-color2">
+                        <h1>Right Panel</h1>
                         <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
                         <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
-                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
                     </div>
                 </div>
+                
             </div>
         </div>
         <!-- /#page-content-wrapper -->
