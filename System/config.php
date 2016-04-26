e0751ecc032b624a23f9151e8828d61c710204a3
gavidhanju <dhanju_dhanju@yahoo.com>
2016-04-26 19:13:01 +0200

    formularis gurwinder

--- System/config.php
+++ System/config.php
@@ -3,9 +3,9 @@
 * Definicio de les constants, que usem per a fer la conexio amb la BD.
 */
     define("HOST","localhost");
-    define("NOMUSUARI","subweb");
-    define("CONTRASENYA","master");
-    define("NOMBDD","db_subweb");
+    define("NOMUSUARI","root");
+    define("CONTRASENYA","1");
+    define("NOMBDD","animaster");
 
     class connexio extends mysqli {
         public function __construct() {
