ffff0a44fe88f4676bb364fa0667d1e03bcde33f
smusaran <marcgomez@iesmontsia.org>
2016-04-27 17:00:39 +0200

    DB 1 hasta partida_usuari

--- System/localhost.sql
+++ System/localhost.sql
@@ -7,225 +7,212 @@
 -- Server version: 5.5.47-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.14
 --
--- Database: `db_subweb`
+-- Database: `db_animaster`
 --
-DROP DATABASE IF EXISTS db_subweb;
-CREATE DATABASE IF NOT EXISTS `db_subweb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-grant all on `db_subweb`.* to 'subweb'@'localhost' identified by 'master';
-USE `db_subweb`;
+DROP DATABASE IF EXISTS db_animaster;
+CREATE DATABASE IF NOT EXISTS `db_animaster` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+grant all on `db_animaster`.* to 'animaster'@'localhost' identified by 'master';
+USE `db_animaster`;
 
 -- --------------------------------------------------------
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 --
--- Table structure for table `Categoria`
+-- Table structure for table `Rol`
 --
-
-CREATE TABLE IF NOT EXISTS `Categoria` (
-  `id_categoria` int(10) NOT NULL AUTO_INCREMENT,
-  `tipus` varchar(32) NOT NULL,
-  PRIMARY KEY (`id_categoria`),
-  UNIQUE KEY `tipus` (`tipus`)
+CREATE TABLE IF NOT EXISTS `Rol` (
+  `id_tipo` int(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_tipo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-INSERT INTO `Categoria` (`id_categoria`, `tipus`) VALUES
-(3, 'Consoles'),
-(2, 'Fotografia'),
-(6, 'Gran electrodomÃ¨stic'),
-(4, 'Imatge i so'),
-(1, 'Informatica'),
-(7, 'Petit electrodomÃ¨stic'),
-(5, 'Telefonia');
+INSERT INTO `Rol` (`id_tipo`,`nombre`) VALUES
+(0, 'Administrador'),
+(1, 'Usuario'),
+(2, 'Master'),
+(3, 'Jugador'),
+(4, 'Expulsado'),
+(5, 'Baneado');
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Licitacio`
+-- Table structure for table `Usuario`
 --
-
-CREATE TABLE IF NOT EXISTS `Licitacio` (
-  `id_usuari` int(10) NOT NULL,
-  `id_subhasta` int(10) NOT NULL,
-  `valor` int(10) NOT NULL,
-  `id_licitacio` int(255) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id_licitacio`),
-  KEY `id_usuari` (`id_usuari`),
-  KEY `id_subhasta` (`id_subhasta`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Factures`
---
-
-CREATE TABLE IF NOT EXISTS `Factura` (
-  `id_producte` int(10) NOT NULL,
-  `id_usuari` int(10),
-  `id_subhasta` int(10) NOT NULL,
-  `valor` varchar(50),
-  `comprat` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_subhasta`),
-  KEY `id_usuari` (`id_usuari`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Producte`
---
-
-CREATE TABLE IF NOT EXISTS `Producte` (
-  `id_producte` int(10) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(32) NOT NULL,
-  `descripcio` varchar(200) NOT NULL,
-  `caracteristiques` varchar(200) NOT NULL,
-  `link_img` varchar(200) NOT NULL,
-  `id_categoria` int(10) NOT NULL,
-  `preu_mercat` int(10) NOT NULL,
-  PRIMARY KEY (`id_producte`),
-  KEY `id_categoria` (`id_categoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Inserts de datos para la tabla `Producte`
---
-
-INSERT INTO `Producte` (`id_producte`, `nom`, `descripcio`, `caracteristiques`, `link_img`, `id_categoria`, `preu_mercat`) VALUES
-(1, 'ps4', 'descripcio1', 'caracteristiques1', 'ps4.png', 3, 400),
-(2, 'nom2', 'des2', 'car2', 'subwoofer.jpg', 1, 200),
-(3, 'nom3', 'des3', 'cr3', 'lavadoraFagor.jpg', 2, 250),
-(4, 'ipad', 'descrip4', 'caract4', 'ipad-air.jpg', 4, 150);
--- --------------------------------------------------------
-
---
--- Table structure for table `Subhasta`
---
-
-CREATE TABLE IF NOT EXISTS `Subhasta` (
-  `id_subhasta` int(10) NOT NULL AUTO_INCREMENT,
-  `id_producte` int(10) NOT NULL,
-  `num_licitacions` int(10) DEFAULT NULL,
-  `temps` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `completada` int(1) NOT NULL,
-  PRIMARY KEY (`id_subhasta`),
-  KEY `id_producte` (`id_producte`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcado de datos para la tabla `Subhasta`
---
-
-INSERT INTO `Subhasta` (`id_subhasta`, `id_producte`, `num_licitacions`, `temps`) VALUES
-(1, 1, 0, '2016-04-11 18:41:59'),
-(2, 4, 100, '2016-04-18 18:44:43'),
-(3, 2, 10, '2016-04-13 18:34:07'),
-(4, 4, 5, '2016-04-15 13:46:12');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Tipus`
---
-
-CREATE TABLE IF NOT EXISTS `Tipus` (
-  `id_tipus` int(10) NOT NULL AUTO_INCREMENT,
-  `rang` varchar(32) NOT NULL,
-  `descripcio` varchar(250) NOT NULL,
-  PRIMARY KEY (`id_tipus`),
-  UNIQUE KEY `rang` (`rang`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-INSERT INTO `Tipus` (`id_tipus`, `rang`, `descripcio`) VALUES
-(1, 'Admin', 'Administrador de subweb'),
-(2, 'User', 'Usuari de subweb');
--- --------------------------------------------------------
-
---
--- Table structure for table `Usuari`
---
-
-CREATE TABLE IF NOT EXISTS `Usuari` (
-  `id_usuari` int(10) NOT NULL AUTO_INCREMENT,
-  `saldo` int(10) NOT NULL,
-  `user` varchar(32) NOT NULL,
-  `email` varchar(40) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Usuario` (
+  `id_usuario` int(10) NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(32) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `nom` varchar(32),
-  `cognom` varchar(32),
-  `dni` varchar(9),
-  `phone` varchar(16),
-  `adreca` varchar(32),
-  `pais` varchar(32),
-  `poble` varchar(32),
-  `provincia` varchar(32),
-  `postal` varchar(5),
-  `id_tipus` int(10) NOT NULL,
+  `nombre` varchar(32),
+  `apellido` varchar(32),
+  `telefono` varchar(16),
+  `imagen` varchar(200),
+  `id_tipo` int(10) NOT NULL,
   PRIMARY KEY (`id_usuari`),
-  UNIQUE KEY `user` (`user`),
-  KEY `id_tipus` (`id_tipus`)
+  UNIQUE KEY `nickname` (`nickname`),
+  KEY `id_tipo` (`id_tipo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
---
--- Constraints for dumped tables
---
+-- --------------------------------------------------------
 
 --
--- Constraints for table `Licitacio`
+-- Table structure for table `Partida`
 --
-ALTER TABLE `Licitacio`
-  ADD CONSTRAINT `Licitacio_1` FOREIGN KEY (`id_usuari`) REFERENCES `Usuari` (`id_usuari`);
-
-ALTER TABLE `Licitacio`
-  ADD CONSTRAINT `Licitacio_2` FOREIGN KEY (`id_subhasta`) REFERENCES `Subhasta` (`id_subhasta`);
-
---
--- Constraints for table `Pagament`
---
-ALTER TABLE `Factura`
-  ADD CONSTRAINT `Pagament_ibfk_1` FOREIGN KEY (`id_usuari`) REFERENCES `Usuari` (`id_usuari`);
-
---
--- Constraints for table `Producte`
---
-ALTER TABLE `Producte`
-  ADD CONSTRAINT `Producte_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `Categoria` (`id_categoria`);
+CREATE TABLE IF NOT EXISTS `Partida` (
+  `id_partida` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(1000),
+  `imagen` varchar(200),
+  `anyo` varchar(5),
+  `nv_sobrenatural` int(1),
+  PRIMARY KEY (`id_partida`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+-- --------------------------------------------------------
 
 --
--- Constraints for table `Subhasta`
+-- Table structure for table `Partida_Usuario`
 --
-ALTER TABLE `Subhasta`
-  ADD CONSTRAINT `Subhasta_ibfk_1` FOREIGN KEY (`id_producte`) REFERENCES `Producte` (`id_producte`);
-
---
--- Constraints for table `Usuari`
---
-ALTER TABLE `Usuari`
-ADD CONSTRAINT `Usuari_ibfk_1` FOREIGN KEY (`id_tipus`) REFERENCES `Tipus` (`id_tipus`);
-
-
-
-INSERT INTO `Usuari` (`id_usuari`, `saldo`, `user`, `email`, `password`, `nom`, `cognom`, `dni`, `phone`, `adreca`, `pais`, `poble`, `provincia`, `postal`, `id_tipus`) VALUES
-(1, 50000, 'Admin', 'subweb@gmail.com', 'eb0a191797624dd3a48fa681d3061212', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
-
-INSERT INTO `Usuari` (`id_usuari`, `saldo`, `user`, `email`, `password`, `nom`, `cognom`, `dni`, `phone`, `adreca`, `pais`, `poble`, `provincia`, `postal`, `id_tipus`) VALUES
-(2, 50000, 'test', 'subweb@gmail.com', 'eb0a191797624dd3a48fa681d3061212', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
-
-
+CREATE TABLE IF NOT EXISTS `Partida_Usuario` (
+  `id_partida` int(10) NOT NULL,
+  `id_usuario` int(10) NOT NULL,
+  PRIMARY KEY `id_partida` (`id_partida`),
+  KEY `id_usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+-- --------------------------------------------------------
 
 --
--- Inserts de datos para la tabla `Licitacio`
+-- Table structure for table `Personaje`
 --
+CREATE TABLE IF NOT EXISTS `Personaje` (
+  `id_personaje` int(10) NOT NULL AUTO_INCREMENT,
+  `id_partida` int(10) NOT NULL,
+  `id_usuario` int(10) NOT NULL,
+  `ventaja1` varchar(80),
+  `ventaja2` varchar(80),
+  `ventaja3` varchar(80),
+  `ventaja4` varchar(80),
+  `ventaja5` varchar(80),
+  `ventaja6` varchar(80),
+  `desventaja1` varchar(80),
+  `desventaja2` varchar(80),
+  `desventaja3` varchar(80),
+  `desventaja4` varchar(80),
+  `nephelim` varchar(80),
+  `cordura` varchar(80),
+  `fama` varchar(80),
+  PRIMARY KEY (`id_personaje`),
+  KEY `id_usuario` (`id_usuario`),
+  KEY `id_partida` (`id_partida`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+-- --------------------------------------------------------
 
-INSERT INTO `Licitacio` (`id_usuari`, `id_subhasta`, `valor`, `id_licitacio`) VALUES
-(1, 1, 15, 1),
-(2, 1, 10, 2),
-(1, 1, 10, 3),
-(1, 1, 5, 4),
-(2, 1, 5, 5),
-(1, 1, 3, 6),
-(1, 1, 3, 7),
-(2, 1, 17, 8);
-
+--
+-- Table structure for table `Ficha`
+--
+CREATE TABLE IF NOT EXISTS `Ficha_Comuna` (
+  `id_ficha` int(10) NOT NULL,
+  `id_personaje` int(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `categoria` varchar(50) NOT NULL,
+  `nivel` int(2) NOT NULL,
+  `edad` int(4) NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `raza` varchar(50) NOT NULL,
+  `pelo` varchar(50) NOT NULL,
+  `ojos` varchar(50) NOT NULL,
+  `puntos_desarrollo` int(4) NOT NULL,
+  `altura` varchar(20) NOT NULL,
+  `peso` varchar(20) NOT NULL,
+  `apariencia` int(2) NOT NULL,
+  `tamanyo` int(2) NOT NULL,
+  `experiencia_actual` int(4) NOT NULL,
+  `experiencia_siguiente_nivel` int(4) NOT NULL,
+  `AGI` int(2) NOT NULL,
+  `CON` int(2) NOT NULL,
+  `DES` int(2) NOT NULL,
+  `FUE` int(2) NOT NULL,
+  `INT` int(2) NOT NULL,
+  `PER` int(2) NOT NULL,
+  `POD` int(2) NOT NULL,
+  `VOL` int(2) NOT NULL,
+  `movimiento` int(3),
+  `regeneracion` int(2),
+  `cansancio` int(2),
+  `idomas` varchar(500),
+  `RF` int(3) NOT NULL,
+  `RE` int(3) NOT NULL,
+  `RV` int(3) NOT NULL,
+  `RM` int(3) NOT NULL,
+  `RP` int(3) NOT NULL,
+  `acrobacias` int(3),
+  `atletismo` int(3),
+  `montar` int(3),
+  `nadar` int(3),
+  `saltar` int(3),
+  `trepar` int(3),
+  `frialdad` int(3),
+  `proezas_fuerza` int(3),
+  `resistir_dolor` int(3),
+  `advertir` int(3),
+  `buscar` int(3),
+  `rastrear` int(3),
+  `animales` int(3),
+  `ciencia` int(3),
+  `herbolaria` int(3),
+  `historia` int(3),
+  `medicina` int(3),
+  `memorizar` int(3),
+  `navegacion` int(3),
+  `ocultismo` int(3),
+  `tasacion` int(3),
+  `valoracion_magica` int(3),
+  `ley` int(3),
+  `tactica` int(3),
+  `estilo` int(3),
+  `intimidar` int(3),
+  `liderazgo` int(3),
+  `persuasion` int(3),
+  `comerciar` int(3),
+  `callejeo` int(3),
+  `etiqueta` int(3),
+  `cerrajeria` int(3),
+  `disfraz` int(3),
+  `ocultarse` int(3),
+  `robo` int(3),
+  `sigilo` int(3),
+  `tramperia` int(3),
+  `venenos` int(3),
+  `arte` int(3),
+  `baile` int(3),
+  `forja` int(3),
+  `truco_manos` int(3),
+  `canto` int(3),
+  `especial1` int(3),
+  `especial2` int(3),
+  `especial3` int(3),
+  `especial4` int(3),
+  `especial5` int(3),
+  `especial6` int(3),
+  `especial8` int(3),
+  `especial9` int(3),
+  `puntos_vida` int(5),
+  `turno` int(3),
+  `ataque` int(3),
+  `parada` int(3),
+  `esquiva` int(3),
+  `llevar_armadura` int(3),
+  `arma1` int(3),
+  `arma2` int(3),
+  `TA_FIL` int(2),
+  `TA_CON` int(2),
+  `TA_PEN` int(2),
+  `TA_CAL` int(2),
+  `TA_FRI` int(2),
+  `TA_ELE` int(2),
+  `TA_ENE` int(2),
+  `tablas` varchar(500),
+  `artes_marciales` varchar(500),
+  PRIMARY KEY `id_ficha` (`id_ficha`),
+  KEY `id_personaje` (`id_personaje`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+-- --------------------------------------------------------
