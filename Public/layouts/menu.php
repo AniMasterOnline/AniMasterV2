fc876e7bf293af1023c35adbc525482d016640d2
NeoDaark <DaarkAssassin@gmail.com>
2016-04-27 13:38:29 +0200

    menu --> user / admin

--- Public/layouts/menu.php
+++ Public/layouts/menu.php
@@ -56,6 +56,14 @@
                     echo '<li><a href="index.php">Home</a></li>';
                     echo '<li class="active"><a href="mesa.php">Mi Mesa</a></li>';
                     echo '<li><a href="theme.php">Theme</a></li>';
+                }else if (strpos($self,"user/")) {
+                    echo '<li><a href="../index.php">Home</a></li>';
+                    echo '<li><a href="../mesa.php">Mi Mesa</a></li>';
+                    echo '<li><a href="../theme.php">Theme</a></li>';
+                }else if (strpos($self,"admin/")) {
+                    echo '<li><a href="../index.php">Home</a></li>';
+                    echo '<li><a href="../mesa.php">Mi Mesa</a></li>';
+                    echo '<li><a href="../theme.php">Theme</a></li>';
                 }else{
                     echo '<li><a href="index.php">Home</a></li>';
                     echo '<li><a href="mesa.php">Mi Mesa</a></li>';
@@ -80,8 +88,29 @@
           <ul class="nav navbar-nav navbar-right"> 
             <?php
             if(isset($_SESSION['user'])){
-                echo '<li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hola, '.ucfirst($value['nickname']).' <span class="caret"></span></a>
+                if (strpos($self,"user/")) {
+                    echo '<li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hola, '.ucfirst($value['nickname']).'! <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                          <li><a target="_blank" href="../user/">Configuración</a></li>
+                          <li role="separator" class="divider"></li>
+                          <li class="dropdown-header">Otros</li>
+                          <li><a target="_self" href="../logout.php"><i class="fa fa-server" aria-hidden="true">&nbsp;</i>Salir</a></li>
+                        </ul>
+                      </li>';
+                }else if (strpos($self,"admin/")) {
+                    echo '<li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hola, '.ucfirst($value['nickname']).'! <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                          <li><a target="_blank" href="../user/">Configuración</a></li>
+                          <li role="separator" class="divider"></li>
+                          <li class="dropdown-header">Otros</li>
+                          <li><a target="_self" href="../logout.php"><i class="fa fa-server" aria-hidden="true">&nbsp;</i>Salir</a></li>
+                        </ul>
+                      </li>';
+                }else{
+                    echo '<li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hola, '.ucfirst($value['nickname']).'! <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                           <li><a target="_blank" href="user/">Configuración</a></li>
                           <li role="separator" class="divider"></li>
@@ -89,6 +118,8 @@
                           <li><a target="_self" href="logout.php"><i class="fa fa-server" aria-hidden="true">&nbsp;</i>Salir</a></li>
                         </ul>
                       </li>';
+                }
+                
             }else{
                 if (strpos($self,"AniMasterV2/signup.php")) {
                     echo '  <li class="active"><a href="signup.php">Sign up</a></li>
@@ -96,6 +127,12 @@
                 }else if (strpos($self,"AniMasterV2/login.php")) {
                     echo '  <li><a href="signup.php">Sign up</a></li>
                             <li class="active"><a href="login.php">Log in</a></li>';
+                }else if (strpos($self,"user/")) {
+                    echo '  <li><a href="../signup.php">Sign up</a></li>
+                            <li><a href="../login.php">Log in</a></li>';
+                }else if (strpos($self,"admin/")) {
+                    echo '  <li><a href="../signup.php">Sign up</a></li>
+                            <li><a href="../login.php">Log in</a></li>';
                 }else{
                     echo '  <li><a href="signup.php">Sign up</a></li>
                             <li><a href="login.php">Log in</a></li>';
