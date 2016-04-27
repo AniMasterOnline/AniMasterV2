104a6fc0f616f62fbd773ec0ad8a88e08a100ce2
NeoDaark <DaarkAssassin@gmail.com>
2016-04-27 19:00:16 +0200

    login form

--- login.php
+++ login.php
@@ -9,21 +9,32 @@ ob_start();
 ?>
 <!-- Body box -->
 <div class="container" >
-    <form class="form-signin" method="POST" action="login.php">
-        <h2 class="form-signin-heading">Login - Animaster Online v2</h2>
-        <label for="inputUser" class="sr-only">Usuario</label>
-        <input type="text"  id="inputUser"  class="form-control" name="user" placeholder="Usuario" required autofocus>
-        
-        <label for="inputPassword" class="sr-only">Contraseña</label>
-        <input type="password" id="inputPassword" class="form-control" name="pass" placeholder="Contraseña" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
+    <form class="form-signin" method="POST">
+        <div class="row">
+            <div class="col-md-12 cinput">
+                <h2 class="form-signin-heading">Inicia sesión en Animaster Online v2</h2>
+            </div>
+            <div class="col-md-12 cinput">
+                <label for="inputUser" class="sr-only">Usuario</label>
+                <input type="text"  id="inputUser"  class="form-control" name="user" placeholder="Usuario" required autofocus>
+            </div>
+            <div class="col-md-12 cinput">
+                <label for="inputPassword" class="sr-only">Contraseña</label>
+                <input type="password" id="inputPassword" class="form-control" name="pass" placeholder="Contraseña" required>
+            </div>
+            <div class="col-md-12 cinput">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                    </label>
+                </div>
+            </div>
+            <div class="col-md-12 cinput">
+                <button class="btn btn-lg btn-primary btn-block" name="login" type="submit">Login</button>
+            </div>
+            <div class="col-md-12" id="alertmsg"></div>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" name="login" type="submit">Login</button>
-        
-      </form>
+    </form>
 </div> <!-- /container -->
 <?php
     require_once('System/Classes/Usuario.php');
