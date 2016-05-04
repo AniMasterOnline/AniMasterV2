ace7b959ddcc8e577467541831dd6a2c0337584c
smusaran <marcgomez@iesmontsia.org>
2016-05-04 20:57:29 +0200

    Caracteristicas_P

--- System/localhost.sql
+++ System/localhost.sql
@@ -238,7 +238,7 @@ CREATE TABLE Habilidades_Esenciales (
 
 
 
-CREATE TABLE Caracteristicas (
+CREATE TABLE Caracteristicas_P (
       base     int NOT NULL,
       bono     int,
       PRIMARY KEY (`base`)
@@ -329,6 +329,13 @@ CREATE TABLE Personaje_Ventaja (
 
 ALTER TABLE Personaje_Ventaja ADD PRIMARY KEY (id_personaje,id_ventaja);
 
+
+CREATE TABLE Ventaja_Efecto (
+      id_ventaja     int NOT NULL,
+      id_efecto     int NOT NULL
+);
+
+ALTER TABLE Categoria_HP ADD PRIMARY KEY (id_categoria,id_HP);
 
 
 CREATE TABLE Personaje_Poderes (
@@ -486,10 +493,10 @@ VALUES
 
 
 --
--- Volcado de datos para la tabla `Caracteristicas`
+-- Volcado de datos para la tabla `Caracteristicas_P`
 --
 
-INSERT INTO `Caracteristicas` (`base`, `bono`) VALUES
+INSERT INTO `Caracteristicas_P` (`base`, `bono`) VALUES
 (1, -30),
 (2, -20),
 (3, -10),
@@ -1715,9 +1722,9 @@ VALUES
 (28, 15, '');
 
 
-INSERT INTO `Habilidades_Esenciales` (`id_habilidad`, `nombre`, `efecto`, `coste`, `gnosis`, `tipo`) 
+INSERT INTO `Habilidades_Esenciales` (`id_habilidad`, `nombre`, `coste`, `gnosis`, `tipo`) 
 VALUES
-(1, 'Resistencia la cansancio', '+2 Cansancio', 10, 0, 'ventaja');
+(1, 'Resistencia la cansancio', 10, 0, 'ventaja');
 
 
 
