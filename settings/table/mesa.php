ebc0df60be7bbbd55f6713c36557f926d529f6ac
NeoDaark <DaarkAssassin@gmail.com>
2016-05-14 00:40:25 +0200

    Mesa + Map --> ok

--- settings/table/mesa.php
+++ settings/table/mesa.php
@@ -158,10 +158,10 @@ include "../../Public/layouts/head.php";?>
                     </ul>
                 </div>
                 <div class="row" >
-                    <div class="col-md-12 bgm-white z-depth-1-bottom" id="mesa-container">
+                    <div class="col-md-12 bgm-white z-depth-1-bottom p-l-0 p-r-0" id="mesa-container">
                         <!-- Tab panes -->
                         <div class="tab-content ">
-                            <div role="tabpanel" class="tab-pane fade in active" id="personaje">
+                            <div role="tabpanel" class="tab-pane fade in active p-l-15 p-r-15" id="personaje">
                                 <div class="row">
                                     <div class="col-md-12">
                                             <h1>Personaje <small>Subtext for header</small></h1>
@@ -171,7 +171,7 @@ include "../../Public/layouts/head.php";?>
                                     </div>
                                 </div>
                             </div>
-                            <div role="tabpanel" class="tab-pane fade " id="habilidades">
+                            <div role="tabpanel" class="tab-pane fade p-l-15 p-r-15" id="habilidades">
                                  <div class="row">
                                     <div class="col-md-12">
                                         <h1>Habilidades</h1>
@@ -181,7 +181,7 @@ include "../../Public/layouts/head.php";?>
                                     </div>
                                 </div>
                             </div>
-                            <div role="tabpanel" class="tab-pane fade " id="combate">
+                            <div role="tabpanel" class="tab-pane fade p-l-15 p-r-15" id="combate">
                                  <div class="row">
                                     <div class="col-md-12">
                                         <h1>Combate</h1>
@@ -191,7 +191,7 @@ include "../../Public/layouts/head.php";?>
                                     </div>
                                 </div>
                             </div>
-                            <div role="tabpanel" class="tab-pane fade " id="equipo">
+                            <div role="tabpanel" class="tab-pane fade p-l-15 p-r-15" id="equipo">
                                  <div class="row">
                                     <div class="col-md-12">
                                         <h1>Equipo</h1>
@@ -201,16 +201,10 @@ include "../../Public/layouts/head.php";?>
                                     </div>
                                 </div>
                             </div>
-                            <div role="tabpanel" class="tab-pane fade " id="mapa">
-                                <div class="row">
-                                  <div class="col-md-12">
-                                      <div id="map">
-                                          
-                                      </div>
-                                  </div>
-                              </div>
+                            <div role="tabpanel" class="tab-pane fade p-0" id="mapa">
+                                <div id='map'></div>
                             </div>
-                            <div role="tabpanel" class="tab-pane fade " id="batalla">
+                            <div role="tabpanel" class="tab-pane fade p-l-15 p-r-15" id="batalla">
                                  <div class="row">
                                     <div class="col-md-12">
                                         <h1>Registro de batalla</h1>
@@ -220,7 +214,7 @@ include "../../Public/layouts/head.php";?>
                                     </div>
                                 </div>
                             </div>
-                            <div role="tabpanel" class="tab-pane fade " id="diario">
+                            <div role="tabpanel" class="tab-pane fade p-l-15 p-r-15" id="diario">
                                  <div class="row">
                                     <div class="col-md-12">
                                         <h1>Diario del jugador</h1>
@@ -236,7 +230,62 @@ include "../../Public/layouts/head.php";?>
             </div>
         </div>
         <!-- /#page-content-wrapper -->
-        
+        <script src="https://maps.googleapis.com/maps/api/js?signed_in=true&key=AIzaSyBkkbD7741_-gMF83eHjO5tgzW5kvx8fj8" type="text/javascript"></script>
+        <script>
+            $(document).ready(function(){
+                initMap();
+                function initMap() {
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                      center: {lat: 0, lng: 0},
+                      zoom: 2,
+                      streetViewControl: false,
+                      mapTypeControl: true,
+                      mapTypeControlOptions: {
+                            mapTypeIds: ['anima']
+                            //mapTypeIds: ['anima']
+                      }
+                    });
+                    var moonMapType = new google.maps.ImageMapType({
+                      getTileUrl: function(coord, zoom) {
+                              var normalizedCoord = getNormalizedCoord(coord, zoom);
+                              if (!normalizedCoord) {
+                                    return null;
+                              }
+                              var bound = Math.pow(2, zoom);
+                              return '../../Public/img/output' +
+                                      '/tile_' + zoom + '_' + normalizedCoord.x + '-' +(normalizedCoord.y) + '.png';
+                      },
+                      tileSize: new google.maps.Size(256, 256),
+                      maxZoom: 5,
+                      minZoom: 1,
+                      radius: 1738000,
+                      name: 'anima'
+                    });
+                    
+
+                    map.mapTypes.set('anima', moonMapType);
+                    map.setMapTypeId('anima');
+                }
+                // Normalizes the coords that tiles repeat across the x axis (horizontally)
+                // like the standard Google map tiles.
+                function getNormalizedCoord(coord, zoom) {
+                        var y = coord.y;
+                        var x = coord.x;
+                        // tile range in one direction range is dependent on zoom level
+                        // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
+                        var tileRange = 100 << zoom;
+                        // don't repeat across y-axis (vertically)
+                        if (y < 0 || y >= tileRange) {
+                          return null;
+                        }
+                        // repeat across x-axis
+                        if (x < 0 || x >= tileRange) {
+                          x = (x % tileRange + tileRange) % tileRange;
+                        }
+                        return {x: x, y: y};
+                }
+            });
+        </script>
 
     </div>
     <!-- /#wrapper -->
